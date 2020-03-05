1bca23aaa28e51e327cdea23649a0c4b34dddf65
rsnazario <rafael.s.nazario@gmail.com>
2020-03-05 13:38:05 -0300

    Bubble_sort_by added with tests

--- bubble.rb
+++ bubble.rb
@@ -1,3 +1,6 @@
+# ############### BUBBLE_SORT Case 1 ############# #
+
+
 #Bubble sort method that sort items in ascending order
 
 def bubble_sort(arr)
@@ -16,18 +19,53 @@ def bubble_sort(arr)
   return arr 
 end
 
-#TEST 1
-result_a = bubble_sort([4, 3, 78, 2, 0, 2])
-print result_a
+#TEST A
+test_1_a = bubble_sort([4, 3, 78, 2, 0, 2])
+print test_1_a
 puts " "
 
-#TEST 2
-result_b = bubble_sort(%w(walter rafael elbie ariel willow lydia greg israel))
-print result_b
+#TEST B
+test_1_b = bubble_sort(%w(walter rafael elbie ariel willow lydia greg israel))
+print test_1_b
 puts " "
 
-#TEST 3
-result_c = bubble_sort([-10, -41, -100, 18, -2, 90, 46, -23])
-print result_c
+#TEST C
+test_1_c = bubble_sort([-10, -41, -100, 18, -2, 90, 46, -23])
+print test_1_c
 puts " "
 
+# ############### BUBBLE_SORT_BY Case 2 ############# #
+
+def bubble_sort_by(arr) 
+  for x in 0...arr.length
+    for y in x...arr.length
+      if yield(arr[x], arr[y]) > 0 
+        arr[x],arr[y] = arr[y],arr[x]
+      end
+    end
+  end
+  return arr
+end
+
+
+test_2_a = bubble_sort_by(["Hi", "Hello", "Hey"]) do
+  |left, right|
+  left.length - right.length
+end
+
+test_2_b = bubble_sort_by(%w(abcde abc a abcd abcdefg ab)) do
+  |left, right|
+  left.length - right.length
+end
+
+test_2_c = bubble_sort_by(%w(lenovo asus dell packardbell macbook razer alienware imac)) do
+|left, right|
+left.length - right.length
+end
+
+print test_2_a
+puts
+print test_2_b
+puts 
+print test_2_c
+puts
