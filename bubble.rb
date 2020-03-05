f4654b68d2c0daa49782af1e553c484c61d0487f
rsnazario <rafael.s.nazario@gmail.com>
2020-03-05 14:32:27 -0300

    Checked Linter

--- bubble.rb
+++ bubble.rb
@@ -1,33 +1,33 @@
 # ############### BUBBLE_SORT Case 1 ############# #
 
 puts "Case 1: Bubble Sort"
-#Bubble sort method that sort items in ascending order
+# Bubble sort method that sort items in ascending order
 
 def bubble_sort(arr)
-  #outer loop that finds the first element in the array
+  # outer loop that finds the first element in the array
   for x in 0...arr.length
-    #inner loop that compares the selected element with other elements
+    # inner loop that compares the selected element with other elements
     for y in x...arr.length
-      #comparison operator that swaps if the element is bigger than the element its been compared
+      # comparison operator that swaps if the element is bigger than the element its been compared
       if arr[x] >= arr[y]
-        #swapping:  arr[x] will change to arr[y] and arr[y] to arr[x] if condition is met
-        arr[x],arr[y] = arr[y],arr[x]
+        # swapping:  arr[x] will change to arr[y] and arr[y] to arr[x] if condition is met
+        arr[x], arr[y] = arr[y], arr[x]
       end
     end
   end
-  #Array returned when sorting is complete
-  return arr 
+  # Array returned when sorting is complete
+  return arr
 end
 
-#TEST A
+# TEST A
 test_1_a = bubble_sort([4, 3, 78, 2, 0, 2])
 print "Test A: #{test_1_a} \n"
 
-#TEST B
+# TEST B
 test_1_b = bubble_sort(%w(walter rafael elbie ariel willow lydia greg israel))
 print "Test B: #{test_1_b} \n"
 
-#TEST C
+# TEST C
 test_1_c = bubble_sort([-10, -41, -100, 18, -2, 90, 46, -23])
 print "Test C: #{test_1_c} \n"
 
@@ -40,39 +40,39 @@ print "Test C: #{test_1_c} \n"
 
 print "\nCase 2: Bubble Sort By - It accepts a block to define the parameter to sort by\n"
 
-def bubble_sort_by(arr) 
-  #outer loop that sets the smallest element on it
+def bubble_sort_by(arr)
+  # outer loop that sets the smallest element on it
   for x in 0...arr.length
-    #inner loop that traverses the array to check each element
+    # inner loop that traverses the array to check each element
     for y in x...arr.length
-      #comparison that takes base on the yield block as specified
-      if yield(arr[x], arr[y]) > 0 
-        #swapping:  arr[x] will change to arr[y] and arr[y] to arr[x] if condition is met
-        arr[x],arr[y] = arr[y],arr[x]
+      # comparison that takes base on the yield block as specified
+      if yield(arr[x], arr[y]) > 0
+        # swapping:  arr[x] will change to arr[y] and arr[y] to arr[x] if condition is met
+        arr[x], arr[y] = arr[y], arr[x]
       end
     end
   end
   return arr
 end
 
-#TEST A
+# TEST A
 test_2_a = bubble_sort_by(["Hi", "Hello", "Hey"]) do
   |left, right|
   left.length - right.length
 end
 
-#TEST B
+# TEST B
 test_2_b = bubble_sort_by(%w(abcde abc a abcd abcdefg ab)) do
   |left, right|
   left.length - right.length
 end
 
-#TEST C
+# TEST C
 test_2_c = bubble_sort_by(%w(lenovo asus dell packardbell macbook razer alienware imac)) do
 |left, right|
 left.length - right.length
 end
 
-print "Test A: #{test_2_a}\n" 
+print "Test A: #{test_2_a}\n"
 print "Test B: #{test_2_b}\n"
 print "Test C: #{test_2_c}\n"
